8a47ea1821049edcf73f8e04b165b993638f51ea
祝万龙 <13298118281@163.com>
2019-03-21 09:52:01 +0800

    Initial Commit

---
+++ Example/Pods/Target Support Files/Pods-ZTBase_Tests/Pods-ZTBase_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZTBase_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ZTBase_Tests
+@end
---
+++ Example/Pods/Target Support Files/ZTBase/ZTBase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZTBase : NSObject
+@end
+@implementation PodsDummy_ZTBase
+@end
